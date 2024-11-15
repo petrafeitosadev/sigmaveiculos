84cf36fc2bc56753267c1f5e496a33f42713a74c
petra feitosa <petrafeitosa@gmail.com>
2024-11-14 21:14:02 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import redesigma
 
+
 app = Flask(__name__)
 
 # Página de login
@@ -9,24 +10,27 @@ def login():
     if request.method == 'POST':
         email = request.form.get('email')
         senha = request.form.get('senha')
-
         # Aqui você faria a lógica de verificação do login
         if email == 'teste@example.com' and senha == '123456':
             return redirect(url_for('sucesso'))
         else:
             return redirect(url_for('erro'))
 
-    return render_template('login.html')
+    return render_template('Principal.html')
 
-# Página de sucesso
-@app.route('/sucesso')
-def sucesso():
-    return render_template('sucesso.html')
+@app.route('/login', methods=['GET', 'POST'])
+def principal():
+    if request.method == 'POST':
+        principal = request.form.get('enttrar')
 
-# Página de erro
-@app.route('/erro')
-def erro():
-    return render_template('erro.html')
-
-if __name__ == '_main_':
-    app.run(debug=True)
+        if principal == True:
+            return redirect('Principal.html')
+        
+@app.route('/Principal', methods= ['POST', 'GET'])
+def principalroute():
+    if request.method == 'POST':
+        veiculos1 = request.form.get('veiculos')
+        vendas1 = request.form.get('vendas')
+        compras1 = request.form.get('compras')
+        pedidos1 = request.form.get('pedidos')
+        
